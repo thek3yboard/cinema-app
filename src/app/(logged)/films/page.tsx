27449e7d0d7d22cc0d9f42df4e8c2019fa5ca075
Juan Ignacio Leiva <juanileiva17@gmail.com>
2024-08-21 23:25:07 -0300

    Update films -> Lazy load and suspense added to grid

--- src/app/(logged)/films/page.tsx
+++ src/app/(logged)/films/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { useState, useEffect, Fragment } from 'react';
-import Image from "next/image";
+import { useState, useEffect, Suspense, lazy } from 'react';
 import { useRouter } from 'next/navigation';
+import Loading from '@/app/(logged)/loading';
+const FilmsGrid = lazy(() => import('@/app/(logged)/films/ui/FilmsGrid'));
 
 type Movie = {
     id: number,
@@ -15,9 +16,9 @@ export default function Films() {
     const [movies, setMovies] = useState<Movie[]>([]);
     const router = useRouter();
 
-    const fetchMovies = () => {
-        fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`, {
-            method: 'GET',
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`, {
+            method: "GET",
             headers: {
                 "Authorization": `Bearer ${process.env.NEXT_PUBLIC_TMDB_BEARER_TOKEN}`
             }
@@ -29,28 +30,18 @@ export default function Films() {
             setMovies(data.results);
         })
         .catch(err => console.error(err));
-    }
-
-    useEffect(() => {
-        fetchMovies();
     }, []);
 
-    const handleClickMovieImage = (movie: Movie) => {
+    const handleClickMovieImage = (movie: Movie): void => {
         router.push(`/film/${movie.id}`);
     }
 
     return (
-        <div className="min-h-full bg-gradient-to-b from-aero-blue to-blueish-gray">
+        <div className="min-h-full">
             <div className="p-16 grid grid-cols-12 gap-4 justify-items-center">
-                {movies.map((m) => {
-                    return (
-                        <Fragment key={m.id}>
-                            <div onClick={() => handleClickMovieImage(m)}>
-                                <Image className="border-2 border-blueish-gray hover:border-[3px] hover:border-green-500" src={`https://image.tmdb.org/t/p/original${m.poster_path}`} alt="Logo" height={100} width={200} />
-                            </div>
-                        </Fragment>
-                    )
-                })}
+                <Suspense fallback={<Loading />}>
+                    <FilmsGrid movies={movies} handleClickMovieImage={handleClickMovieImage} />
+                </Suspense>
             </div>
         </div>
     );
---
+++ src/app/(logged)/films/ui/FilmsGrid.tsx
@@ -0,0 +1,30 @@
+import { Fragment } from "react";
+import Image from "next/image";
+
+type Movie = {
+    id: number,
+    title: string,
+    overview: string,
+    poster_path: string
+}
+
+type Props = {
+    movies: Movie[],
+    handleClickMovieImage: (movie: Movie) => void;
+}
+
+export default function FilmsGrid({ movies, handleClickMovieImage }: Props) {
+    return (
+        <>
+            {movies.map((m) => {
+                return (
+                    <Fragment key={m.id}>
+                        <div onClick={() => handleClickMovieImage(m)}>
+                            <Image className="border-2 border-blueish-gray hover:border-[3px] hover:border-green-500" src={`https://image.tmdb.org/t/p/original${m.poster_path}`} alt="Logo" height={100} width={200} />
+                        </div>
+                    </Fragment>
+                )
+            })}
+        </>
+    )
+}
