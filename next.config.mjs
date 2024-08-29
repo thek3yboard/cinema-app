2df26287c54472ade8b63957352d3e83b26cbc9b
Juan Ignacio Leiva <juanileiva17@gmail.com>
2024-08-29 09:45:17 -0300

    Update next.config -> env variables added

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
     images: {
         domains: ['tmdb.org', 'themoviedb.org', 'image.tmdb.org'],
     },
+    env: {
+        NEXT_PUBLIC_TMDB_API_KEY: process.env.NEXT_PUBLIC_TMDB_API_KEY,
+        NEXT_PUBLIC_TMDB_BEARER_TOKEN: process.env.NEXT_PUBLIC_TMDB_BEARER_TOKEN
+    }
 };
 
 export default nextConfig;
