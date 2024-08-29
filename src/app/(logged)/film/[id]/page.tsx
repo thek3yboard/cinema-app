e72578be6cd4aed5dedbec0ac1f73802273322d9
Juan Ignacio Leiva <jleiva@cognisys.com.ar>
2024-08-29 00:51:08 -0300

    Update /film/:id -> loading spinner logic added until image fully loaded

--- src/app/(logged)/film/[id]/page.tsx
+++ src/app/(logged)/film/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect, Suspense, lazy } from 'react'; 
-import Loading from '@/app/(logged)/loading';
+import Loading from '@/app/ui/loading';
 import { MovieData } from "@/types/types";
 const FilmUI = lazy(() => import('@/app/(logged)/film/[id]/ui/FilmUI'));
 
--- src/app/(logged)/film/[id]/ui/FilmUI.tsx
+++ src/app/(logged)/film/[id]/ui/FilmUI.tsx
@@ -1,5 +1,6 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import Image from "next/image";
+import Loading from "@/app/ui/loading";
 import { MovieData, ProductionCompanies } from "@/types/types";
 
 type Props = {
@@ -7,24 +8,32 @@ type Props = {
 }
 
 export default function FilmUI({ movieData }: Props) {
+    const [isImageLoaded, setIsImageLoaded] = useState(false);
+
     return (
         <div className="min-h-full">
             <div className="flex flex-col items-start">
-                <Image className="z-0 movie-img" src={`https://image.tmdb.org/t/p/original${movieData?.backdrop_path}`} alt="Backdrop Path Image" width={1300} height={800} />
-                <h1 className="mt-[-5rem] w-[1250px] z-20 pl-8 text-white text-6xl font-bold">{movieData?.title && `${movieData?.title} (${movieData?.release_date.substring(0,4)})`}</h1>
-                <div className="w-2/4 pl-8">
-                    <p className="text-white mt-4 text-lg">{movieData?.overview}</p>
-                </div>
-                <div className="w-full pl-8">
-                    {movieData?.production_companies.map((pc: ProductionCompanies) => {
-                        return (
-                            pc.logo_path !== null && 
-                            <Fragment key={pc.id}>
-                                <Image className="m-4 inline-block" src={`https://image.tmdb.org/t/p/original${pc.logo_path}`} alt="Production Company" width={90} height={90} />
-                            </Fragment>
-                        )
-                    })}
-                </div>
+                <Image onLoad={() => setIsImageLoaded(true)} className="z-0 movie-img" src={`https://image.tmdb.org/t/p/original${movieData?.backdrop_path}`} alt="Backdrop Path Image" width={1300} height={800} />
+                { isImageLoaded ? 
+                    <>
+                        <h1 className="mt-[-5rem] w-[1250px] z-20 pl-8 text-white text-6xl font-bold">{movieData?.title && `${movieData?.title} (${movieData?.release_date.substring(0,4)})`}</h1>
+                        <div className="w-2/4 pl-8">
+                            <p className="text-white mt-4 text-lg">{movieData?.overview}</p>
+                        </div>
+                        <div className="w-full pl-8">
+                            {movieData?.production_companies.map((pc: ProductionCompanies) => {
+                                return (
+                                    pc.logo_path !== null && 
+                                    <Fragment key={pc.id}>
+                                        <Image className="m-4 inline-block" src={`https://image.tmdb.org/t/p/original${pc.logo_path}`} alt="Production Company" width={90} height={90} />
+                                    </Fragment>
+                                )
+                            })}
+                        </div>
+                    </>
+                :
+                    <Loading />
+                }
             </div>  
         </div>
     )
