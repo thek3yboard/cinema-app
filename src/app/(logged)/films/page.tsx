d902aadc7e84fad82c6d9906af6208005c9b8a87
Juan Ignacio Leiva <jleiva@cognisys.com.ar>
2024-08-28 22:34:59 -0300

    Update films -> page number state and label created

--- src/app/(logged)/films/page.tsx
+++ src/app/(logged)/films/page.tsx
@@ -8,13 +8,14 @@ const FilmsGrid = lazy(() => import('@/app/(logged)/films/ui/FilmsGrid'));
 
 export default function Films() {
     const [movies, setMovies] = useState<Movie[]>([]);
-    const [currentPages, setCurrentPages] = useState([1,2]);
+    const [currentApiPages, setCurrentApiPages] = useState([1,2]);
+    const [page, setPage] = useState(1);
     const router = useRouter();
 
     useEffect(() => {
         const fetchFirstPage = async () => {
             try {
-                let res = await fetch(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${currentPages[0]}&api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`, {
+                let res = await fetch(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${currentApiPages[0]}&api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`, {
                     method: "GET",
                     headers: {
                         "Authorization": `Bearer ${process.env.NEXT_PUBLIC_TMDB_BEARER_TOKEN}`
@@ -31,7 +32,7 @@ export default function Films() {
 
         const fetchSecondPage = async () => {
             try {
-                let res = await fetch(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${currentPages[1]}&api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`, {
+                let res = await fetch(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${currentApiPages[1]}&api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`, {
                     method: "GET",
                     headers: {
                         "Authorization": `Bearer ${process.env.NEXT_PUBLIC_TMDB_BEARER_TOKEN}`
@@ -54,29 +55,31 @@ export default function Films() {
         };
         
         fetchBoth();
-    }, [currentPages]);
+    }, [currentApiPages]);
 
     const handleClickMovieImage = (movie: Movie): void => {
         router.push(`/film/${movie.id}`);
     }
 
     const handleClickPrevPage = () => {
-        setCurrentPages([currentPages[0]-2, currentPages[1]-2]);
+        setCurrentApiPages([currentApiPages[0]-2, currentApiPages[1]-2]);
+        setPage(p => p - 1);
     }
 
     const handleClickNextPage = () => {
-        setCurrentPages([currentPages[0]+2, currentPages[1]+2]);
+        setCurrentApiPages([currentApiPages[0]+2, currentApiPages[1]+2]);
+        setPage(p => p + 1);
     }
 
     return (
         <div className="min-h-full">
             <div className=" px-8 pt-4 grid grid-columns-10-135px gap-3 justify-items-center justify-center">
-                <Suspense key={currentPages[0]} fallback={<Loading />}>
+                <Suspense key={currentApiPages[0]} fallback={<Loading />}>
                     <FilmsGrid movies={movies} handleClickMovieImage={handleClickMovieImage} />
                 </Suspense>
             </div>
-            <div className="absolute bottom-8 left-1/2 translate-x-[-36px] flex mt-2 justify-center">
-                <button disabled={currentPages[0] === 1} onClick={handleClickPrevPage} className="disabled:opacity-25 active:translate-y-[2px]">
+            <div className="absolute bottom-8 left-1/2 translate-x-[-4em] flex mt-2 justify-center items-center">
+                <button disabled={currentApiPages[0] === 1} onClick={handleClickPrevPage} className="disabled:opacity-25 active:translate-y-[2px]">
                     <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-arrow-left" width="36" height="36" viewBox="0 0 24 24" stroke-width="2" stroke="#e4fde1" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <path d="M12 21a9 9 0 1 0 0 -18a9 9 0 0 0 0 18" />
@@ -85,6 +88,7 @@ export default function Films() {
                         <path d="M12 8l-4 4" />
                     </svg>
                 </button>
+                <p className="text-white mx-2">Page {page}</p>
                 <button onClick={handleClickNextPage} className="active:translate-y-[2px]">
                     <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-arrow-right" width="36" height="36" viewBox="0 0 24 24" stroke-width="2" stroke="#e4fde1" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
