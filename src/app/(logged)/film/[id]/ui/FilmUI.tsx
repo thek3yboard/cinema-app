5a932fc90a1d666dd4ecc09ca23cc19db56b2de2
Juan Ignacio Leiva <juanileiva17@gmail.com>
2024-08-31 17:51:25 -0300

    Images set with priority true for preloading

--- src/app/(logged)/film/[id]/ui/FilmUI.tsx
+++ src/app/(logged)/film/[id]/ui/FilmUI.tsx
@@ -13,7 +13,7 @@ export default function FilmUI({ movieData }: Props) {
     return (
         <div className="min-h-full">
             <div className="flex flex-col items-start">
-                <Image onLoad={() => setIsImageLoaded(true)} className="z-0 movie-img" src={`https://image.tmdb.org/t/p/original${movieData?.backdrop_path}`} alt="Backdrop Path Image" width={1300} height={800} />
+                <Image onLoad={() => setIsImageLoaded(true)} className="z-0 movie-img" priority={true} src={`https://image.tmdb.org/t/p/original${movieData?.backdrop_path}`} alt="Backdrop Path Image" width={1300} height={800} />
                 { isImageLoaded ? 
                     <>
                         <h1 className="mt-[-5rem] w-[1250px] z-20 pl-8 text-white text-6xl font-bold">{movieData?.title && `${movieData?.title} (${movieData?.release_date.substring(0,4)})`}</h1>
@@ -25,7 +25,7 @@ export default function FilmUI({ movieData }: Props) {
                                 return (
                                     pc.logo_path !== null && 
                                     <Fragment key={pc.id}>
-                                        <Image className="m-4 inline-block" src={`https://image.tmdb.org/t/p/original${pc.logo_path}`} alt="Production Company" width={90} height={90} />
+                                        <Image className="m-4 inline-block" priority={true} src={`https://image.tmdb.org/t/p/original${pc.logo_path}`} alt="Production Company" width={90} height={90} />
                                     </Fragment>
                                 )
                             })}
