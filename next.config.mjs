0c6f909b6c9d169fb8d52588d8f6d2a961a4631b
Juan Ignacio Leiva <juanileiva17@gmail.com>
2024-09-01 00:43:36 -0300

    Update next config -> image optimization disabled

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     images: {
         domains: ['tmdb.org', 'themoviedb.org', 'image.tmdb.org'],
+        unoptimized: true
     },
     env: {
         NEXT_PUBLIC_TMDB_API_KEY: process.env.NEXT_PUBLIC_TMDB_API_KEY,
