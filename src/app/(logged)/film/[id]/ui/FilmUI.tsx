2b716024ce737ea53db00ae55f1e95b52958c5cc
Juan Ignacio Leiva <juanileiva17@gmail.com>
2024-09-02 22:24:38 -0300

    Update FilmUI -> more responsive breakpoints

--- src/app/(logged)/film/[id]/ui/FilmUI.tsx
+++ src/app/(logged)/film/[id]/ui/FilmUI.tsx
@@ -13,13 +13,13 @@ export default function FilmUI({ movieData }: Props) {
     return (
         <div>
             <div className="flex flex-col items-start">
-                <div className="relative w-[100vw] h-[35vh] sm:h-[50vh] lg:w-[1300px] lg:h-[731px]">
+                <div className="relative w-[100vw] h-[215px] sm:h-[360px] md:h-[450px] lg:h-[575px] xl:w-[1000px] xl:h-[560px] 2xl:w-[1300px] 2xl:h-[731px]">
                     <Image onLoad={() => setIsImageLoaded(true)} className={`z-0 movie-img ${isImageLoaded === false && `hidden` }`} priority={true} src={`https://image.tmdb.org/t/p/original${movieData?.backdrop_path}`} alt="Backdrop Image" layout="fill" />
                 </div>
                 { isImageLoaded ? 
                     <>
-                        <h1 className="xl:mt-[-5rem] xl:w-[1250px] z-10 px-3 xl:pl-8 text-white text-3xl sm:text-5xl xl:text-6xl font-bold">{movieData?.title && `${movieData?.title} (${movieData?.release_date.substring(0,4)})`}</h1>
-                        <div className="w-screen xl:w-2/4 px-3 xl:pl-8">
+                        <h1 className="xl:mt-[-5rem] xl:w-[1000px] 2xl:w-[1250px] z-10 px-3 xl:pl-8 text-white text-3xl sm:text-5xl 2xl:text-6xl font-bold">{movieData?.title && `${movieData?.title} (${movieData?.release_date.substring(0,4)})`}</h1>
+                        <div className="w-screen xl:w-[1000px] 2xl:w-[1250px] px-3 xl:pl-8">
                             <p className="text-white mt-4 sm:text-lg">{movieData?.overview}</p>
                         </div>
                         <div className="xl:w-full xl:pl-8">
