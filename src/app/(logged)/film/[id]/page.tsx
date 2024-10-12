418088e302f0a7987663ccae387c8aa0b22082d9
Juan Ignacio Leiva <juanileiva17@gmail.com>
2024-10-11 23:31:14 -0300

    Update film/id -> new API call to get movie trailer

--- src/app/(logged)/film/[id]/page.tsx
+++ src/app/(logged)/film/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect, Suspense, lazy } from 'react'; 
-import Loading from '@/app/ui/loading';
+import Loading from '@/app/ui/Loading';
 import { MovieData } from "@/types/types";
 const FilmUI = lazy(() => import('@/app/(logged)/film/[id]/ui/FilmUI'));
 
@@ -9,23 +9,43 @@ export default function Film({ params }: { params: { id: number } }) {
     const [movieData, setMovieData] = useState<MovieData>();
 
     useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/${params.id}language=en-US&api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`, {
-            method: "GET",
-            headers: {
-                "Authorization": `Bearer ${process.env.NEXT_PUBLIC_TMDB_BEARER_TOKEN}`
+        const fetchData = async () => {
+            try {
+                let res = await fetch(`https://api.themoviedb.org/3/movie/${params.id}language=en-US&api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`, {
+                    method: "GET",
+                    headers: {
+                        "Authorization": `Bearer ${process.env.NEXT_PUBLIC_TMDB_BEARER_TOKEN}`
+                    }
+                });
+                
+                let data = await res.json();
+
+                res = await fetch(`https://api.themoviedb.org/3/movie/${params.id}/videos?language=en-US&api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`, {
+                    method: "GET",
+                    headers: {
+                        "Authorization": `Bearer ${process.env.NEXT_PUBLIC_TMDB_BEARER_TOKEN}`
+                    }
+                });
+                
+                const video = await res.json();
+
+                for (const v of video.results) {
+                    if(v.type === 'Trailer') {
+                        data = { ...data, video_id: v.key };
+                    }
+                }
+
+                setMovieData(data);
+            } catch (error) {
+                console.error(error)
             }
-        })
-        .then(res => { 
-            return res.json(); 
-        })
-        .then(data => { 
-            setMovieData(data);
-        })
-        .catch(err => console.error(err));
+        }
+
+        fetchData();
     }, [params.id]);
 
     return (
-        <Suspense key={params?.id} fallback={<Loading/>}>
+        <Suspense key={params!.id} fallback={<Loading/>}>
             <FilmUI movieData={movieData!} />
         </Suspense>
     );
