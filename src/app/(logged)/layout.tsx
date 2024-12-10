f445e0caf3c3632ee4cb27e511af93efad176068
Juan Ignacio Leiva <juanileiva17@gmail.com>
2024-12-10 20:56:46 -0300

    Fix Logged Layout -> changes in description
    - Toggle button added to show search bar and filter button
    - Now it's possible to search for movies/shows while looking at one's details
    - Visual changes in search bar, filter button and pages buttons

--- src/app/(logged)/layout.tsx
+++ src/app/(logged)/layout.tsx
@@ -5,7 +5,7 @@ import { MediaContext, initialPage, initialCurrentApiPages, initialSort } from "
 import { SortType, Movie, Show } from '@/types/types';
 import { orderOptions, sortByOptions } from '@/assets/filtersData';
 import { usePathname, useRouter } from 'next/navigation';
-import { Search, Sliders, AlignJustify } from 'lucide-react'
+import { Search, Sliders, AlignJustify, ChevronDown, ChevronUp } from 'lucide-react'
 import { Select, SelectItem, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, 
     Navbar, NavbarBrand, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, NavbarContent, NavbarItem, Link, useDisclosure } from "@nextui-org/react";
 import Image from "next/image";
@@ -28,11 +28,26 @@ export default function LoggedLayout({
     const router = useRouter();
     const pathname = usePathname();
     const {isOpen, onOpen, onClose} = useDisclosure();
+    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+    const [isParametersMenuOpen, setIsParametersMenuOpen] = useState<boolean>(false);
+    const [isPagesMenuOpen, setIsPagesMenuOpen] = useState<boolean>(false);
 
     const navbarItems = [
         "Movies",
         "Shows",
     ];
+
+    const handleMenuToggle = (open: boolean) => {
+        setIsMenuOpen(open);
+    }
+
+    const handleParametersMenuToggle = (open: boolean) => {
+        setIsParametersMenuOpen(open);
+    };
+
+    const handlePagesMenuToggle = (open: boolean) => {
+        setIsPagesMenuOpen(open);
+    };
 
     const handleClickPrevPage = () => {
         setMovies([]);
@@ -82,6 +97,9 @@ export default function LoggedLayout({
     }
 
     const handleSetFilters = () => {
+        setIsMenuOpen(false);
+        handleParametersMenuToggle(false);
+
         const selectedOrder = orderOptions.find((option) => option.key === orderRef.current)!;
         const selectedSort = sortByOptions.find((option) => option.key === sortRef.current)!;
         setSort({ ...sort, key: selectedSort.key, label: selectedSort.label,
@@ -108,6 +126,9 @@ export default function LoggedLayout({
 
     const handleClickSearch = async () => {
         try {
+            setIsMenuOpen(false);
+            handleParametersMenuToggle(false);
+
             let URL = '';
 
             switch(pathname) {
@@ -166,15 +187,34 @@ export default function LoggedLayout({
         <MediaContext.Provider value={{ page, setPage, currentApiPages, setCurrentApiPages, handleClickPrevPage, handleClickNextPage, sort, movies, setMovies, shows, setShows }}>
         <>
             <div ref={screenRef} className="h-screen flex flex-col overflow-y-auto bg-gradient-to-r from-[#192a49] from-1% via-[#3f577c] via-50% to-[#192a49] to-99%">
-                <div className="z-20 sticky top-0">
-                    <Navbar className="h-auto bg-gradient-to-r from-aero-blue to-blueish-gray border-b-2 border-slate-700">
+                <div className="z-20 sticky top-0 border-b-2 border-slate-700">
+                    <Navbar disableAnimation={true} isMenuOpen={isMenuOpen} onMenuOpenChange={handleMenuToggle} className="h-auto bg-gradient-to-r from-aero-blue to-blueish-gray">
                         <NavbarContent className="md:hidden" justify="start">
                             <NavbarBrand>
                                 <Image className='min-w-32 mb-2' src={logo} alt="Logo" width={128} />
                             </NavbarBrand>
                         </NavbarContent>
                         <NavbarContent className="md:hidden" justify="end">
-                            <NavbarMenuToggle icon={<AlignJustify />} />
+                            <NavbarMenuToggle 
+                                isDisabled={isPagesMenuOpen}
+                                isSelected={isParametersMenuOpen}
+                                onChange={handleParametersMenuToggle}
+                                icon={isParametersMenuOpen ? 
+                                    <button style={{ color: '#192a49' }} className='p-2 rounded-md bg-slate-200'><ChevronUp /></button> 
+                                    : 
+                                    <ChevronDown />
+                                }
+                            />
+                            <NavbarMenuToggle 
+                                isDisabled={isParametersMenuOpen}
+                                isSelected={isPagesMenuOpen}
+                                onChange={handlePagesMenuToggle}
+                                icon={isPagesMenuOpen ? 
+                                    <button style={{ color: '#192a49' }} className='p-2 rounded-md bg-slate-200'><AlignJustify /></button> 
+                                    : 
+                                    <AlignJustify />
+                                }
+                            />
                         </NavbarContent>
                         <NavbarContent className="hidden md:flex gap-6" justify="center">
                             <NavbarBrand>
@@ -200,29 +240,42 @@ export default function LoggedLayout({
                                 </NavbarItem>
                             ))}
                         </NavbarContent>
-                        <NavbarMenu className='p-5'>
-                            {navbarItems.map((item, index) => (
-                                <NavbarMenuItem key={`${item}-${index}`}>
-                                    {pathname.includes(item.toLowerCase()) ?
-                                        <Link
-                                            className="text-orange-400 w-full font-semibold"
-                                            href={`/${item.toLowerCase()}`}
-                                            size="lg"
-                                        >
-                                            {item}
-                                        </Link>
-                                        :
-                                        <Link
-                                            className="text-nyanza text-xl font-semibold"
-                                            href={`/${item.toLowerCase()}`}
-                                            size="lg"
-                                        >
-                                            {item}
-                                        </Link>
-                                    }
+                        <NavbarMenu className='max-h-fit mt-[2px] p-5'>
+                            { isPagesMenuOpen ?
+                                navbarItems.map((item, index) => (
+                                    <NavbarMenuItem key={`${item}-${index}`}>
+                                        {pathname.includes(item.toLowerCase()) ?
+                                            <Link
+                                                className="text-orange-400 text-xl font-semibold"
+                                                href={`/${item.toLowerCase()}`}
+                                            >
+                                                {item}
+                                            </Link>
+                                            :
+                                            <Link
+                                                className="text-nyanza text-xl font-semibold"
+                                                href={`/${item.toLowerCase()}`}
+                                            >
+                                                {item}
+                                            </Link>
+                                        }
+                                    </NavbarMenuItem>
+                                ))
+                            : isParametersMenuOpen &&
+                                <NavbarMenuItem>
+                                    <div className='md:hidden w-full flex justify-between'>
+                                        <span className="flex items-center rounded-l-sm w-full h-10 bg-blueish-gray">
+                                            <input type='text' onChange={(e) => handleChangeSearch(e)} onKeyDown={(e) => handleKeydownSearch(e)} className='w-[calc(100%-30px)] pl-2 ml-[2px] h-full bg-blueish-gray' />
+                                            <button className='bg-lapis-lazuli ml-[1px] h-full rounded-r-sm' onClick={handleClickSearch}>
+                                                <Search className='mx-2 max-h-6' />
+                                            </button>
+                                        </span>
+                                        <Button disabled={pathname.includes('/shows') === true} className={`max-w-fit bg-lapis-lazuli ml-2 rounded-sm ${pathname.includes('/shows') && 'opacity-50'}`} key="full" onClick={handleOpen}>
+                                            <Sliders />
+                                        </Button>
+                                    </div>
                                 </NavbarMenuItem>
-                                
-                            ))}
+                            }
                         </NavbarMenu>
                         <span className="max-md:hidden flex items-center w-2/3 h-10 bg-blueish-gray rounded-[3px]">
                             <input type='text' onChange={(e) => handleChangeSearch(e)} onKeyDown={(e) => handleKeydownSearch(e)} className='w-[calc(100%-30px)] pl-2 ml-[2px] h-full bg-blueish-gray' />
@@ -234,43 +287,32 @@ export default function LoggedLayout({
                             <Sliders className='max-h-6' />
                         </button>
                     </Navbar>
-                    <div className='md:hidden w-full flex items-start'>
-                        <span className="flex items-center w-2/3 h-10 bg-blueish-gray">
-                            <input type='text' onChange={(e) => handleChangeSearch(e)} onKeyDown={(e) => handleKeydownSearch(e)} className='w-[calc(100%-30px)] pl-2 ml-[2px] h-full bg-blueish-gray' />
-                            <button onClick={handleClickSearch}>
-                                <Search className='mx-2 max-h-6' />
-                            </button>
-                        </span>
-                        <Button className="w-1/3 bg-lapis-lazuli border-l-1 border-b-2 border-blueish-gray rounded-none" key="full" onClick={handleOpen}>
-                            <Sliders className='max-h-6' />
-                        </Button>
-                    </div>
                 </div>
                 { (pathname === "/movies" || pathname === "/shows") ?
                     <>
-                    <div className="grow content-center my-4 2xl:overflow-hidden">
-                        {children}
-                    </div>
-                    <div className="h-6 flex justify-center">
-                        <footer className="h-6 text-nyanza">Copyright © 2024 Juan Ignacio Leiva</footer>
-                    </div>
+                        <div className="grow content-center my-4 2xl:overflow-hidden">
+                            {children}
+                        </div>
+                        <div className="h-6 flex justify-center">
+                            <footer className="h-6 text-nyanza">Copyright © 2024 Juan Ignacio Leiva</footer>
+                        </div>
                     </>
                 :
                     <>
-                    <div className="grow">
-                        {children}
-                    </div>
-                    <div className="flex h-6 justify-center">
-                        <footer className="h-6 text-nyanza">Copyright © 2024 Juan Ignacio Leiva</footer>
-                    </div> 
+                        <div className="grow">
+                            {children}
+                        </div>
+                        <div className="flex h-6 justify-center">
+                            <footer className="h-6 text-nyanza">Copyright © 2024 Juan Ignacio Leiva</footer>
+                        </div> 
                     </>
                 }
             </div>
             <Modal 
-            size="full"
-            isOpen={isOpen} 
-            onClose={onClose}
-            className="bg-gradient-to-b from-blueish-gray via-[#3f577c] to-blueish-gray"
+                size="full"
+                isOpen={isOpen} 
+                onClose={onClose}
+                className="bg-gradient-to-b from-blueish-gray via-[#3f577c] to-blueish-gray"
             >
                 <ModalContent>
                 {(onClose) => (
