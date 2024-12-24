32139164d5a524370053413fc5a8a2a601213c13
Juan Ignacio Leiva <juanileiva17@gmail.com>
2024-12-24 15:25:21 -0300

    Update Language -> new language latam spanish

--- src/app/components/ui/LanguageSelect.tsx
+++ src/app/components/ui/LanguageSelect.tsx
@@ -1,7 +1,7 @@
-import React, { ChangeEventHandler, useContext, useState } from 'react';
+import React, { ChangeEventHandler, useContext } from 'react';
 import { Select, SelectItem } from '@nextui-org/react';
+import { US, ES, MX } from 'country-flag-icons/react/3x2'
 import { MediaContext } from '@/app/(logged)/MediaContext';
-import { LanguageType } from '@/types/types';
 
 type Props = {
     handleChangeLanguage: ChangeEventHandler<HTMLSelectElement>,
@@ -13,7 +13,10 @@ export const languageOptions = [
         key: 'en-US', label: 'English'
     },
     {
-        key: 'es-ES', label: 'Spanish'
+        key: 'es-ES', label: 'Español'
+    },
+    {
+        key: 'es-MX', label: 'Español'
     }
 ]
 
@@ -25,18 +28,43 @@ export default function LanguageSelect({ handleChangeLanguage, smallDevice }: Pr
             key="language"
             color="default"
             label="Language"
-            variant={`${smallDevice === true ? 'bordered' : 'flat'}`}
             radius="sm"
-            value={language.label}
-            placeholder={language.label}
-            className={`${smallDevice === true ? 'bg-blueish-gray rounded-xl' : 'max-md:hidden w-48'}`}
+            renderValue={() => {
+                switch(language.key) {
+                    case 'en-US':
+                        return <span className='flex flex-row gap-2' key={language.key}>{language.label} <US className='w-4' /></span>
+                    case 'es-ES':
+                        return <span className='flex flex-row gap-2' key={language.key}>{language.label} <ES className='w-4' /></span>
+                    case 'es-MX':
+                        return <span className='flex flex-row gap-2' key={language.key}>{language.label} <MX className='w-4' /></span>
+                    default:
+                        return <span key={language.key}>{language.label}</span>
+                }
+            }}
+            placeholder={
+                <span className='flex flex-row gap-2' key={language.key}>{language.label} 
+                    { language.key === 'en-US' ?
+                        <US className='w-4' />
+                    : language.key === 'es-ES' ?
+                        <ES className='w-4' />
+                    :
+                        <MX className='w-4' />
+                    }
+                </span> as any
+            }
+            className={`${smallDevice === true ? '' : 'max-md:hidden w-48'}`}
             onChange={handleChangeLanguage}
-            selectedKeys={language.key}
         >
             {languageOptions.map((option) => (
             <SelectItem key={option.key}>
                 <div className='flex flex-row items-center gap-2'>
-                    {option.label}
+                    {option.label} {option.key === 'en-US' ? 
+                            <US className='w-4' /> 
+                        : option.key === 'es-ES' ?
+                            <ES className='w-4' />
+                        :
+                            <MX className='w-4' />
+                        }
                 </div>
             </SelectItem>
             ))}
