4b236cf074fab291cc07e68606166263d6642d74
Juan Ignacio Leiva <juanileiva17@gmail.com>
2025-02-07 15:29:02 -0300

    Initial internationalization config with i18n

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+ 
+const withNextIntl = createNextIntlPlugin();
+ 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
@@ -18,5 +22,5 @@ const nextConfig = {
         ]
     }
 };
-
-export default nextConfig;
+ 
+export default withNextIntl(nextConfig);
