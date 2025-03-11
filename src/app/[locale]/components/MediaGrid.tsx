3de4c973d9316c8914ff382fd205f3c58a72ff9a
Juan Ignacio Leiva <juanileiva17@gmail.com>
2025-03-11 12:30:27 -0300

    Logic to show the correct photo attribute

--- src/app/[locale]/components/MediaGrid.tsx
+++ src/app/[locale]/components/MediaGrid.tsx
@@ -36,7 +36,11 @@ export default function MediaGrid({ media, handleClickMediaImage, imagesLoaded, 
                 return (
                     <Fragment key={m.id}>
                         <div className='flex items-center' onClick={() => handleClickMediaImage(m)}>
-                            <Image onLoad={() => handleSumLoadedImages()} className={`h-[190px] border-2 border-blueish-gray hover:border-[3px] hover:border-green-500 ${imagesLoaded === false && `hidden` }`} priority={true} src={`${m.poster_path !== null ? `https://image.tmdb.org/t/p/original${m.poster_path}` : 'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg' }`} alt="Media Poster" height={190} width={130} />
+                            {m.poster_path !== undefined ?
+                                <Image onLoad={() => handleSumLoadedImages()} className={`h-[190px] border-2 border-blueish-gray hover:border-[3px] hover:border-green-500 ${imagesLoaded === false && `hidden` }`} priority={true} src={`${m.poster_path !== null ? `https://image.tmdb.org/t/p/original${m.poster_path}` : 'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg' }`} alt="Media Poster" height={190} width={130} />
+                            :
+                                <Image onLoad={() => handleSumLoadedImages()} className={`h-[190px] border-2 border-blueish-gray hover:border-[3px] hover:border-green-500 ${imagesLoaded === false && `hidden` }`} priority={true} src={`${m.profile_path !== null ? `https://image.tmdb.org/t/p/original${m.profile_path}` : 'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg' }`} alt="Media Poster" height={190} width={130} />
+                            }
                         </div>
                     </Fragment>
                 )
