c804a52ea671daafe37d9318ee273e0468f67478
Juan Ignacio Leiva <juanileiva17@gmail.com>
2025-03-20 11:55:01 -0300

    ExpandableText UI component created

--- src/app/[locale]/components/ui/ExpandableText.tsx
+++ src/app/[locale]/components/ui/ExpandableText.tsx
@@ -1,23 +1,31 @@
 import { useState } from 'react';
 
 type Props = {
-    text: string;
-    maxLength: number;
+  text: string;
+  maxLength: number;
+  initialParentHeight: number;
 }
 
-const ExpandableText = ({ text, maxLength = 100 }: Props) => {
+const ExpandableText = ({ text, maxLength = 100, initialParentHeight }: Props) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   if (text.length <= maxLength) return <p>{text}</p>;
 
   return (
-    <p className='text-sm'>
-      {isExpanded ? text : `${text.slice(0, maxLength)}... `}
-      <br />
-      <button onClick={() => setIsExpanded(!isExpanded)} className="text-blue-500">
+    <div style={{ maxHeight: `${initialParentHeight}px` }} className="flex flex-col">
+      {isExpanded ? (
+        <div className="overflow-y-auto">
+          <p className="text-sm mr-4">{text}</p>
+        </div>
+      ) : (
+        <p className="text-sm text-ellipsis overflow-hidden">
+          {`${text.slice(0, maxLength)}...`}
+        </p>
+      )}
+      <button onClick={() => setIsExpanded(!isExpanded)} className="text-blue-500 mt-2">
         {isExpanded ? 'Less' : 'More'}
       </button>
-    </p>
+    </div>
   );
 };
 
