da5c942435d4058cf42ad9741ba68fd15eb68468
Juan Ignacio Leiva <juanileiva17@gmail.com>
2025-03-26 21:15:55 -0300

    Person filmography data handling improved

--- src/app/[locale]/(logged)/people/[id]/page.tsx
+++ src/app/[locale]/(logged)/people/[id]/page.tsx
@@ -14,14 +14,14 @@ export default function Person({ params }: { params: { id: number } }) {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                let personResponse = await fetch(`https://api.themoviedb.org/3/person/${params.id}?language=${language.key}&api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`, {
+                let personDataResponse = await fetch(`https://api.themoviedb.org/3/person/${params.id}?language=${language.key}&api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`, {
                     method: "GET",
                     headers: {
                         "Authorization": `Bearer ${process.env.NEXT_PUBLIC_TMDB_BEARER_TOKEN}`
                     }
                 });
                 
-                let personResponseData = await personResponse.json();
+                let personDetailsData = await personDataResponse.json();
 
                 let personWorkResponse = await fetch(`https://api.themoviedb.org/3/person/${params.id}/combined_credits?language=${language.key}&api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`, {
                     method: "GET",
@@ -32,15 +32,30 @@ export default function Person({ params }: { params: { id: number } }) {
                 
                 let personWorkData = await personWorkResponse.json();
 
-                let cast = personWorkData.cast.filter((m) => (m.poster_path !== null && m.vote_count >= 100));
+                let work = null;
 
-                cast = cast.filter(obra => 
-                    !(obra.hasOwnProperty('name') && (obra.name.includes('Show') || obra.name.includes('Live'))) // Exclude tv shows
-                    && !(obra.media_type === "tv" && obra.character.includes("Self")) // Exclude 'tv' + 'Self'
-                    || (obra.media_type === "movie" && obra.character.includes("Self")) // Keep 'movie' + 'Self'
-                );
+                if(personDetailsData.known_for_department === 'Directing') {
+                    work = personWorkData.crew.filter((m) => m.poster_path !== null && m.vote_count >= 100);
 
-                cast = cast.sort((a: any, b: any) => {
+                    work = work.filter(obra => 
+                        !(obra.hasOwnProperty('name') && (obra.name.includes('Show') || obra.name.includes('Live'))) 
+                        && !(obra.media_type === "tv" && obra.hasOwnProperty('character') && obra.character.includes("Self"))
+                        /* || (obra.media_type === "movie" && obra.character.includes("Self")) */
+                    );
+                } else {
+                    work = personWorkData.cast.filter((m) => m.poster_path !== null && m.vote_count >= 100);
+
+                    work = work.filter(obra => 
+                        !(obra.hasOwnProperty('name') && (obra.name.includes('Show') || obra.name.includes('Live'))) 
+                        && !(obra.media_type === "tv" && obra.hasOwnProperty('character') && obra.character.includes("Self"))
+                        /* || (obra.media_type === "movie" && obra.character.includes("Self")) */
+                    );
+                }
+
+                work = Array.from(new Set(work.map(item => item.id)))
+                .map(id => work.find(item => item.id === id));
+            
+                work = work.sort((a: any, b: any) => {
                     const getDate = (item: any) => {
                         if (item.media_type === 'movie') {
                             return new Date(item.release_date).getTime();
@@ -54,10 +69,10 @@ export default function Person({ params }: { params: { id: number } }) {
                     return aDate - bDate;
                 });
 
-                personWorkData.cast = cast;
+                //personWorkData.cast = work;
 
-                setPersonData(personResponseData);
-                setPersonWork(personWorkData.cast);
+                setPersonData(personDetailsData);
+                setPersonWork(work);
             } catch (error) {
                 console.error(error)
             }
