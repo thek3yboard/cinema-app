02efc8aa65741ab3db8fbbffb0f084ed87ccc43a
Juan Ignacio Leiva <juanileiva17@gmail.com>
2025-03-26 23:32:49 -0300

    PersonUI -> Initial mobile responsiveness

--- src/app/[locale]/components/PersonUI.tsx
+++ src/app/[locale]/components/PersonUI.tsx
@@ -33,16 +33,16 @@ export default function PersonUI({ personData, personWork }: Props) {
         <>{isLoading ?
             <Loading />
         :
-            <div className='flex flex-row h-full pt-4 pl-4'>
-                <div className="flex flex-col items-start p-6 rounded-md w-[15%] h-full gap-3 bg-slate-800">
+            <div className='flex md:flex-row flex-col h-full max-md:gap-4 pt-4 pl-4 max-md:pr-4 max-md:pb-4'>
+                <div className="flex flex-col items-start p-6 rounded-md md:w-[15%] w-full h-full gap-3 bg-slate-800">
                     <label className='text-white text-2xl font-bold'>{personData.name}</label>
                     <NextImage 
                     priority={true}
-                    className="w-fit border-1.5 border-blueish-gray hover:border-gray-300"
+                    className="md:w-fit border-1.5 border-blueish-gray hover:border-gray-300"
                     src={`${personData?.profile_path !== null ? `https://image.tmdb.org/t/p/original${personData?.profile_path}` : `https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg`}`}
                     alt="Profile Image"
-                    height={342}
-                    width={273}
+                    height={171}
+                    width={136}
                     />
                     <div className='grow' ref={biographyDivRef}>
                         <ExpandableText text={personData!.biography} maxLength={400} initialParentHeight={initialHeight} />
